a6c9d73882bca765d02846c9fc0793b383f389e7
Maria Stepura <stepuramasha1234@gmail.com>
2025-01-02 11:22:10 +0800

    feat: common-character, count-cats, delete-digit, dream-team

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let num = String(n).split('').map(Number);
+  let res = num.map((el,i) => {
+    let newArr = num.slice();
+    newArr.splice(i,1);
+    return Number(newArr.join(''))})
+  return Math.max(...res);
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let str = '';
+  members = members.filter((el) => {
+    return (typeof el === 'string');
+  }).map((el) => {
+    return el.trim().toUpperCase();
+  }).sort();
+  members.forEach(element => {
+    str += element[0];
+  });
+  return str;
 }
 
 module.exports = {
   createDreamTeam
 };
+
+console.log(createDreamTeam([
+  ['David Abram'],
+  ['Robin Attfield'],
+  'Thomas Berry',
+  ['Paul R.Ehrlich'],
+  'donna Haraway',
+  ' BrIaN_gOodWiN  ',
+  {
+    0: 'Serenella Iovino'
+  },
+  'Erazim Kohak',
+  '  val_plumwood',
+]));
